d688376585076fa308c8a8303c7371d8c4606a75
Seval Karaman <sevalkaraman653@gmail.com>
2022-09-22 23:18:27 +0300

    wall creator

---
+++ Assets/Scripts/WallCreator.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCreator : MonoBehaviour
+{
+    public GameObject cube;
+    public Transform parent;
+    public int xCount = 3;
+    public int yCount = 4;
+    public float size = 1;
+
+    private List<GameObject> _cubes = new List<GameObject>();
+
+    [ContextMenu("Place Cubes")]
+    public void PlaceCubes()
+    {
+        DeleteCubes();
+        SetCubesPositions();
+
+        foreach (Transform child in parent)
+        {
+            _cubes.Add(child.gameObject);
+        }
+
+    }
+    [ContextMenu("Delete Cubes")]
+    public void DeleteCubes()
+    {
+        foreach (GameObject child in _cubes)
+        {
+            DestroyImmediate(child);
+        }
+    }
+    private void SetCubesPositions()
+    {
+        for (int i = 0; i < yCount; i++)
+        {
+            for (int j = 0; j < xCount; j++)
+            {
+                var newCube = Instantiate(cube, parent);
+                newCube.transform.localScale = Vector3.one * size;
+                newCube.transform.localPosition = new Vector3( j * size, i * -size, 0f);
+            }
+        }
+    }
+    
+}
